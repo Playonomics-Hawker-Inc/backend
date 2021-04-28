85657e5f4abd762d52c5ed5cc512cc073127c0be
hawkerlabs <hawkerlabs@gmail.com>
2021-04-27 23:58:14 -0700

    first commit

---
+++ eureka/src/main/kotlin/com/playonomics/eureka/EurekaApplication.kt
@@ -0,0 +1,36 @@
+package com.playonomics.eureka
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@EnableEurekaServer
+@SpringBootApplication
+class EurekaApplication : WebMvcConfigurer {
+	@EnableWebSecurity
+	internal class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+		@Throws(Exception::class)
+		override fun configure(http: HttpSecurity) {
+			http.csrf().ignoringAntMatchers("/eureka/**")
+			super.configure(http)
+		}
+	}
+
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**").allowedHeaders("Authorization", "content-type", "x-auth-token")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").maxAge(3600L)
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(EurekaApplication::class.java, *args)
+		}
+	}
+}
+
---
+++ gateway/src/main/kotlin/com/playonomics/gateway/GatewayApplication.kt
@@ -0,0 +1,15 @@
+package com.playonomics.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
