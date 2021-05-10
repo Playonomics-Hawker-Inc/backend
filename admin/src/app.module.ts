95018135c981ce28931f99cd29ef2f0d40832cad
hawkerlabs <hawkerlabs@gmail.com>
2021-05-09 23:20:24 -0700

    category changes

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -9,6 +9,7 @@ import { UploadModule } from './image/upload.module';
 import { ExperienceModule } from './experience/experience.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UploadModule,
     ExperienceModule,
+    CategoryModule,
     UserModule,
     AuthModule,
   ],
