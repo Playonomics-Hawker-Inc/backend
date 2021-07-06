dd8b595f9adb3055ba5d492d9432e9ad5d2dd7b5
sunil mohanty <sunilmohanty724@gmail.com>
2021-07-06 22:59:08 +0530

    payment gateway backend

---
+++ payment/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { PaymentModule } from './payment/payment.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }),
+ 
+    PaymentModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
